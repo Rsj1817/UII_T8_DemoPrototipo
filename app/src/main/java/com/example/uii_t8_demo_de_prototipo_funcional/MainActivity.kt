c0ef165b2ea92bf2ce187f507f764d74302913db
Rigoberto Sanchez Jimenez <20243ds185@utez.edu.mx>
2025-10-16 10:47:40 -0600

    Primer commit

---
+++ app/src/main/java/com/example/uii_t8_demo_de_prototipo_funcional/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.uii_t8_demo_de_prototipo_funcional
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.uii_t8_demo_de_prototipo_funcional.ui.theme.UII_T8_Demo_de_prototipo_funcionalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UII_T8_Demo_de_prototipo_funcionalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UII_T8_Demo_de_prototipo_funcionalTheme {
+        Greeting("Android")
+    }
+}
